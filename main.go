e8ceb2f17cb938dd79d818709a8de219a060d0f9
Ryano0oceros <getryanfallon@gmail.com>
2022-11-30 13:22:15 -0500

    end 58

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
+func main() {
+	resp, err := http.Get("https://google.com")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	fmt.Println(resp)
 }
